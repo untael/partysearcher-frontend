58d5926e9bdca833ac3619ecb1807401046dab16
untael <pashamironow@gmail.com>
2018-05-11 18:27:51 +0300

    Layout

---
+++ src/components/Universal/Layout.demo.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <vm-layout>
+
+        </vm-layout>
+    </div>
+</template>
+
+
+<script>
+
+  import VmLayout from './Layout'
+
+  export default {
+    components:{
+      VmLayout,
+    }
+  }
+</script>
---
+++ src/components/Universal/Layout.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="body">
+        fff
+    </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'VmLayout',
+  }
+</script>
+
+<style>
+    .body {
+        background-color: lightskyblue;
+    }
+</style>
